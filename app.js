7f9b5650848b74aefd06056762f4672f6547a145
Burhanmrj <Burhanmrj@users.noreply.github.com>
2020-11-04 11:59:11 +0530

    added advanced search button and map section

--- app.js
+++ app.js
@@ -9,12 +9,30 @@ app.set('view engine', 'ejs');
 const aboutcontent="We are CS undergrad from DTU currently in 3rd year making this website as a Project under Professor Rahul Gupta.";
 const contactcontent="Here are some of the ways you can contact us:";
 const api="1d8c7268fc6cf66b6eb5b37c449398c7";
-
+var $ = require('jquery')
 var temperature={};
 var icon={};
-var parsedData={};
+let parsedData={};
+var lat={};
+var lon={};
+var url2={};
+var today = new Date();
+var day=today.getDay();
+var week=['SUN','MON','TUE','WED','THR','FRI','SAT'];
+var months=['Jan','Feb','Mar','Apr','May','Jun',"Jul",'Aug','Sep','Oct','Nov','Dec'];
+console.log(today.getHours())
 
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+let nodeGeocoder = require('node-geocoder');
+let options = {
+  provider: 'openstreetmap'
+};
+ 
+let geoCoder = nodeGeocoder(options);
 
+  
 app.get("/",function(req,res){
 	res.render("header",{	name:null,
 							temperature: null,
@@ -41,6 +59,12 @@ app.get("/Docs",function(req,res){
 	res.render("Docs")
 
 })
+app.get("/Advanced",function(req,res){
+	res.render("Advanced")
+})
+app.get("/leaflet-owm",function(req,res){
+	res.render("leaflet-owm")
+})
 app.post("/",function(req,res){
 
 
@@ -59,6 +83,7 @@ app.post("/",function(req,res){
 				
 				console.log(parsedData)
 				name=parsedData.name;
+				coordinates=parsedData.coord;
 				temp_min = "Min: "+parsedData.main.temp_min+"°C";
 				temp_max= "Max: "+parsedData.main.temp_max+"°C";
 				description=parsedData.weather[0].description;
@@ -84,8 +109,36 @@ app.post("/",function(req,res){
 
 										});
 });
+app.post("/Advanced",function(req,resp){
 
-
+	var aquiredLocation=req.body.locationName;
+	aquiredLocation=capitalizeFirstLetter(aquiredLocation);
+	const units = "metric";
+	geoCoder.geocode(aquiredLocation)
+  .then((res)=> {
+    lat=res[0].latitude;
+    lon=res[0].longitude;
+    
+  
+  	console.log(lat,lon);
+ 	url2="https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=hourly,minutely&units="+units+"&appid="+api;
+	console.log(url2)
+	https.get(url2,function(response){
+		response.on("data", function(data){
+				parsedData =JSON.parse(data);
+				console.log(parsedData)
+				resp.render("Advanced",{parsedData:parsedData,
+										aquiredLocation:aquiredLocation,
+										week:week,
+										day:day,
+										months:months
+									})
+			});
+	});
+	 });
+  
+});
+	
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
